3eaa6758234dd2c42dd1a456f59ac0bc4d5385f4
Samm <samm@Samms-MacBook-Pro.local>
2022-04-08 05:03:34 +1000

    design and nav

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import logo from '../logo.png';
+
+const Navbar = () => {
+  return (
+    <header>
+      <img src={logo} alt="logo" />
+      <h1>Cryption</h1>
+    </header>
+  )
+}
+
+export default Navbar;
